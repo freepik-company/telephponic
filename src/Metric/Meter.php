b1e65d457218aeb1381e930e77c9bfe91be728a5
José Antonio Muriano Criado <jamuriano@freepik.com>
2023-08-28 19:33:55 +0200

    feat(WIP): add meter concept

---
+++ src/Metric/Meter.php
@@ -0,0 +1,10 @@
+1<?php
+
+declare(strict_types=1);
+
+namespace GR\Telephponic\Metric;
+
+class Meter
+{
+
+}
---
+++ src/Metric/Type/Histogram.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GR\Telephponic\Metric\Type;
+
+class Histogram
+{
+
+}
